89acf97437c4d24bc2ec874a18c3a593beca0964
nastasia-99 <gagina.anastasi@gmail.com>
2024-11-01 21:00:39 +0300

    55:06

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,37 +1,62 @@
-import React from 'react';
+"use client";
+import React, { useState } from "react";
 //*my imports
 import styles from "./links.module.css";
-import NavLink from './navLink/navLink';
-
-const Links = () => {
+import NavLink from "./navLink/navLink";
 
 const links = [
-
-    {
-        title: "Homepage",
-        path: "/",
-    },
-    {
-        title: "About",
-        path: "/about",
-    },
-    {
-        title: "Contact",
-        path: "/contact",
-    },
-    {
-        title: "Blog",
-        path: "/blog",
-    },
+  {
+    title: "Homepage",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
 ];
 
-  return (
-    <div className={styles.links}>
-      {links.map((link =>(
-      <NavLink item={link} key={link.title}/>  
-  )))}
-    </div>
-  )
-}
+const Links = () => {
+  const [open, setOpen] = useState(false);
 
-export default Links
+  //*temprorary
+  const session = true;
+  const isAdmin = true;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}{" "}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
+        )}
+      </div>
+      <button onClick={() => setOpen((prev) => !prev)}>Menu</button>
+      { open && (
+        <div className={styles.mobileLinks}>
+        {links.map((link)=> (
+          <NavLink item={link} key={link.title}/>
+        ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Links;
+
+
